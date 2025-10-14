6d269d9972de7d7379c364a46ea758be672b437a
Bryan <bryanfonsecafernandes@gmail.com>
2025-10-14 00:11:39 +0000

    Navios posicionados na vertical e horizontal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,21 @@ int main() {
         tabuleiro[i][j] = 3;  
     }
 }
+    //Posicionando os Navios!
+    //Navio horizontal (linha 3, colunas 3-5)
+    for (int j = 2; i < 3; j++)
+    {
+        tabuleiro [2][j] = 3;
+    }
+    //Navio vertical (coluna 7, linhas 5-7)
+    for (int i = 5; i < 8; i++)
+    {
+        tabuleiro[i][6] = 3;
+    }
+    
+    //Imprimir o tabuleiro
+    printf("--- TABULEIRO DE BATALHA NAVAL! ---\n");
+    
     
     return 0;
 }
