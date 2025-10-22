9fdab8f8f690c1671eb2f81534440698b49d9557
Bryan <bryanfonsecafernandes@gmail.com>
2025-10-22 01:27:05 +0000

    Formato do Cone

--- batalhaNavalMestre.c
+++ batalhaNavalMestre.c
@@ -15,3 +15,14 @@ int main() {
     tabuleiro[2][3] = NAVIO;
     tabuleiro[5][5] = NAVIO;
     tabuleiro[6][5] = NAVIO;
+        // Aplicando habilidade Cone em (1,1)
+    for(int i=-2; i<=2; i++){
+        for(int j=-2; j<=2; j++){
+            int x = 1 + i;
+            int y = 1 + j;
+            if(x>=0 && x<LINHA && y>=0 && y<COLUNA){
+                if(j >= -i && j <= i && tabuleiro[x][y]!=NAVIO)
+                    tabuleiro[x][y] = HABILIDADE;
+            }
+        }
+    }
